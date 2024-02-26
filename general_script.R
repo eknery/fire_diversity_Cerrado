486c71d647994a0fcf5567b3b475c06b106a330e
Eduardo Koerich Nery <91353128+eknery@users.noreply.github.com>
2024-02-26 15:50:42 -0300

    update project

--- general_script.R
+++ general_script.R
@@ -1,40 +1,173 @@
-### input data
-## env data
-env_data = read.csv("0_data/env_data.csv", sep=",", h=T)
+########################## LOADING LIBRARIES ##################################
+if (!require("tidyverse")) install.packages("tidyverse"); require("tidyverse")
+if (!require("ggplot2")) install.packages("ggplot2"); library("ggplot2")
+if (!require("nlme")) install.packages("nlme"); library("nlme")
+if (!require("philentropy")) install.packages("philentropy"); library("philentropy")
+if (!require("ape")) install.packages("ape"); library("ape")
 
-## spp data
+############################### LOADING DATA ###################################
+
+### environmental data
+comm_data = read.csv("0_data/comm_data.csv", sep=",", h=T)
+head(comm_data)
+
+### species data
 spp_data = read.csv("0_data/spp_data.csv", sep=",", h=T, stringsAsFactors = T)
-spp_data = spp_data[1:15]
 head(spp_data)
 
+############################ PROCESSING DATA ##################################
 
-##  create site_plot variable
-# for env data
-site_plot = paste(env_data$site, env_data$plot, sep="_")
-env_data = data.frame(site_plot, env_data)
-# for spp_data
-site_plot = paste(spp_data$site, spp_data$plot, sep="_")
-spp_data = data.frame(site_plot, spp_data)
+### removing non-used info from species data
+spp_data  =  spp_data %>% 
+  filter(!is.na(sp))
 
-unique(site_plot)
+### transforming fire
+comm_data = comm_data %>% 
+  mutate(trans_fire = fire_frequency^(1/3) )
 
-## abundance per plot spp df
-abundace_plot = data.frame(table(spp_data$site_plot, spp_data$sp))
-# remove unusal plot naming
-abundace_plot = abundace_plot[abundace_plot$Var1 %in% env_data$site_plot,]
+################################# FIRE REGIME #################################
 
-env_data$site_plot[!env_data$site_plot %in% abundace_plot$Var1]
-
-unique(abundace_plot$Var1)
-
-# add fire values 
-fire = c()
-for (i in 1:nrow(abundace_plot)){
-  plot_index = which(abundace_plot$Var1[i] == env_data$site_plot)
-  fire = c(fire, env_data$fire_frequency[plot_index] )
+### check residual
+check_resid = function(model){
+  N = model$dims$N
+  resid_n = resid(model)[1:N]
+  shapiro.test(resid_n)
 }
 
-data.frame(abundace_plot, fire)
-str(abundace_plot)
+##### GLS
 
-# iti vs ca
+### my model
+gls_fire1 = gls(data = comm_data,
+    trans_fire ~ seasonal_precipitation + soil_PC1 + soil_PC2, 
+    method = "REML",
+    correlation = corGaus(form = ~ longitude + latitude, nugget = TRUE))
+
+summary(gls_fire1)
+plot(gls_fire1)
+check_resid(model = gls_fire1)
+
+### alternative model
+gls_fire2 = gls(data = comm_data,
+               trans_fire ~ site, 
+               method = "REML",
+               correlation = corGaus(form = ~ longitude + latitude, nugget = TRUE))
+
+summary(gls_fire2)
+plot(gls_fire2)
+check_resid(model = gls_fire2)
+
+############################## COMMUNITY METRICS ###############################
+
+##### richeness
+
+hist(comm_data$richeness)
+
+### simple model
+gls_rich1 = gls(data = comm_data,
+                richness ~ fire_frequency, 
+                method = "REML",
+                correlation = corGaus(form = ~ longitude + latitude, nugget = TRUE))
+
+summary(gls_rich1)
+plot(gls_rich1)
+check_resid(model = gls_rich1)
+
+### full model
+gls_rich2 = gls(data = comm_data,
+    richness ~ fire_frequency + seasonal_precipitation + soil_PC1 + soil_PC2, 
+    method = "REML",
+    correlation = corGaus(form = ~ longitude + latitude, nugget = TRUE))
+
+summary(gls_rich2)
+plot(gls_rich2)
+check_resid(model = gls_rich1)
+
+
+##### species diversity
+
+hist(comm_data$fisher)
+
+### simple model
+gls_div1 = gls(data = comm_data,
+                fisher ~ fire_frequency, 
+                method = "REML",
+                correlation = corGaus(form = ~ longitude + latitude, nugget = TRUE))
+
+summary(gls_div1)
+plot(gls_div1)
+check_resid(model = gls_div1)
+
+### full model
+gls_div2 = gls(data = comm_data,
+                fisher ~ fire_frequency + seasonal_precipitation + soil_PC1 + soil_PC2, 
+                method = "REML",
+                correlation = corGaus(form = ~ longitude + latitude, nugget = TRUE))
+
+summary(gls_div2)
+plot(gls_div2)
+check_resid(model = gls_div2)
+
+
+
+########################### SPECIES COMPOSITION ############################
+
+### all species names
+spp_names = as.character(unique(spp_data$sp))
+
+### species presence per plot
+plot_spp = spp_data %>% 
+  unite("plot_id", site, plot, sep="_", remove = FALSE) %>% 
+  mutate(plot_id = as.character(plot_id)) %>% 
+  group_by(plot_id) %>% 
+  reframe(sp = as.character(unique(sp)), presence = 1) %>% 
+  pivot_wider(names_from = sp,
+              names_expand = T,
+              values_from = presence,
+              values_fill = as.integer(0)
+  )
+
+### transforming in matrix
+plot_spp_mtx = as.matrix(plot_spp[,-1])
+rownames(plot_spp_mtx) = plot_spp$plot_id
+
+### fire per plot
+plot_fire = comm_data %>% 
+  unite("plot_id", site, plot, sep="_", remove = FALSE) %>% 
+  mutate(plot_id = as.character(plot_id)) %>% 
+  dplyr::select(any_of(c("plot_id", "fire_frequency") ) )
+
+### transforming in matrix
+plot_fire_mtx = as.matrix(plot_fire[,-1])
+rownames(plot_fire_mtx) = plot_fire$plot_id
+
+### fire per plot
+plot_env = comm_data %>% 
+  unite("plot_id", site, plot, sep="_", remove = FALSE) %>% 
+  mutate(plot_id = as.character(plot_id)) %>% 
+  dplyr::select(any_of(c("plot_id", 
+                         "fire_frequency", 
+                         "seasonal_precipitation",
+                         "soil_PC1",
+                         "soil_PC2"
+                         ) 
+                       ) 
+                )
+
+### transforming in matrix
+plot_env_mtx = as.matrix(plot_env[,-1])
+rownames(plot_env_mtx) = plot_env$plot_id
+
+
+### dissimilarity matrices
+spp_dist = distance(plot_spp_mtx, "sorensen")
+fire_dist = distance(plot_fire_mtx, "euclidean")
+env_dist = distance(plot_env_mtx, "euclidean")
+
+ape::mantel.test(spp_dist, env_dist, 
+                 nperm = 999,
+                 alternative = "greater",
+                 graph = T)
+
+
+
+
