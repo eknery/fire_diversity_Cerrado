1f77a00048e50c7e1fbe2e8ee6ff52125a4e96c4
Eduardo Koerich Nery <91353128+eknery@users.noreply.github.com>
2024-02-27 23:14:16 -0300

    update project

--- general_script.R
+++ general_script.R
@@ -1,9 +1,14 @@
 ########################## LOADING LIBRARIES ##################################
+
+### old package
+install.packages("0_data/mvpart_1.6-2.tar.gz", repos = NULL, type = "source")
+require("mvpart")
+
+### maintained packages
 if (!require("tidyverse")) install.packages("tidyverse"); require("tidyverse")
 if (!require("ggplot2")) install.packages("ggplot2"); library("ggplot2")
 if (!require("nlme")) install.packages("nlme"); library("nlme")
-if (!require("philentropy")) install.packages("philentropy"); library("philentropy")
-if (!require("ape")) install.packages("ape"); library("ape")
+if (!require("vegan")) install.packages("vegan"); library("vegan")
 
 ############################### LOADING DATA ###################################
 
@@ -12,18 +17,26 @@ comm_data = read.csv("0_data/comm_data.csv", sep=",", h=T)
 head(comm_data)
 
 ### species data
-spp_data = read.csv("0_data/spp_data.csv", sep=",", h=T, stringsAsFactors = T)
-head(spp_data)
+ind_data = read.csv("0_data/ind_data.csv", sep=",", h=T, stringsAsFactors = T)
+head(ind_data)
 
 ############################ PROCESSING DATA ##################################
 
 ### removing non-used info from species data
-spp_data  =  spp_data %>% 
+ind_data  =  ind_data %>% 
   filter(!is.na(sp))
 
 ### transforming fire
 comm_data = comm_data %>% 
   mutate(trans_fire = fire_frequency^(1/3) )
+
+### scaled predictors
+scale_comm = comm_data %>% 
+  mutate(fire_frequency =scale(fire_frequency),
+         seasonal_precipitation= scale(seasonal_precipitation),
+         soil_PC1 = scale(soil_PC1),
+         soil_PC2 = scale(soil_PC2)
+  )
 
 ################################# FIRE REGIME #################################
 
@@ -37,85 +50,83 @@ check_resid = function(model){
 ##### GLS
 
 ### my model
-gls_fire1 = gls(data = comm_data,
+gls_fire1 = gls(
+    data = scale_comm,
     trans_fire ~ seasonal_precipitation + soil_PC1 + soil_PC2, 
     method = "REML",
-    correlation = corGaus(form = ~ longitude + latitude, nugget = TRUE))
+    correlation = corLin(form = ~ longitude + latitude))
 
 summary(gls_fire1)
 plot(gls_fire1)
 check_resid(model = gls_fire1)
 
-### alternative model
-gls_fire2 = gls(data = comm_data,
-               trans_fire ~ site, 
-               method = "REML",
-               correlation = corGaus(form = ~ longitude + latitude, nugget = TRUE))
-
-summary(gls_fire2)
-plot(gls_fire2)
-check_resid(model = gls_fire2)
 
 ############################## COMMUNITY METRICS ###############################
 
-##### richeness
+##### species richeness
 
 hist(comm_data$richeness)
 
-### simple model
-gls_rich1 = gls(data = comm_data,
-                richness ~ fire_frequency, 
-                method = "REML",
-                correlation = corGaus(form = ~ longitude + latitude, nugget = TRUE))
+### species richness model
+gls_rich1 = gls(
+    data = scale_comm,
+    richness ~ fire_frequency + seasonal_precipitation + soil_PC1 + soil_PC2, 
+    method = "REML",
+    correlation = corLin(form = ~ longitude + latitude))
 
 summary(gls_rich1)
 plot(gls_rich1)
 check_resid(model = gls_rich1)
 
-### full model
-gls_rich2 = gls(data = comm_data,
-    richness ~ fire_frequency + seasonal_precipitation + soil_PC1 + soil_PC2, 
+### species abundance model
+gls_abund1 = gls(data = comm_data,
+    density ~ fire_frequency + seasonal_precipitation + soil_PC1 + soil_PC2, 
     method = "REML",
-    correlation = corGaus(form = ~ longitude + latitude, nugget = TRUE))
+    correlation = corLin(form = ~ longitude + latitude))
 
-summary(gls_rich2)
-plot(gls_rich2)
-check_resid(model = gls_rich1)
-
+summary(gls_abund1)
+plot(gls_abund1)
+check_resid(model = gls_abund1)
 
 ##### species diversity
 
 hist(comm_data$fisher)
 
-### simple model
-gls_div1 = gls(data = comm_data,
-                fisher ~ fire_frequency, 
+### species diversity model
+gls_div1 = gls(data = scale_comm,
+                fisher ~ fire_frequency + seasonal_precipitation + soil_PC1 + soil_PC2, 
                 method = "REML",
-                correlation = corGaus(form = ~ longitude + latitude, nugget = TRUE))
+                correlation = corLin(form = ~ longitude + latitude))
 
 summary(gls_div1)
 plot(gls_div1)
 check_resid(model = gls_div1)
 
-### full model
-gls_div2 = gls(data = comm_data,
-                fisher ~ fire_frequency + seasonal_precipitation + soil_PC1 + soil_PC2, 
-                method = "REML",
-                correlation = corGaus(form = ~ longitude + latitude, nugget = TRUE))
+##### species composition
 
-summary(gls_div2)
-plot(gls_div2)
-check_resid(model = gls_div2)
+hist( comm_data$floristic_PCo2 )
+
+### species compostion model
+gls_comp1 = gls(data = scale_comm,
+               floristic_PCo2 ~ fire_frequency + seasonal_precipitation + soil_PC1 + soil_PC2, 
+               method = "REML",
+               correlation = corLin(form = ~ longitude + latitude))
+
+summary(gls_comp1)
+plot(gls_comp1)
+check_resid(model = gls_comp1)
 
 
+ggplot(comm_data)+
+geom_point(aes(x= floristic_PCo1, y=floristic_PCo2, size= fire_frequency))
 
 ########################### SPECIES COMPOSITION ############################
 
 ### all species names
-spp_names = as.character(unique(spp_data$sp))
+spp_names = as.character(unique(ind_data$sp))
 
 ### species presence per plot
-plot_spp = spp_data %>% 
+plot_spp = ind_data %>% 
   unite("plot_id", site, plot, sep="_", remove = FALSE) %>% 
   mutate(plot_id = as.character(plot_id)) %>% 
   group_by(plot_id) %>% 
@@ -130,15 +141,6 @@ plot_spp = spp_data %>%
 plot_spp_mtx = as.matrix(plot_spp[,-1])
 rownames(plot_spp_mtx) = plot_spp$plot_id
 
-### fire per plot
-plot_fire = comm_data %>% 
-  unite("plot_id", site, plot, sep="_", remove = FALSE) %>% 
-  mutate(plot_id = as.character(plot_id)) %>% 
-  dplyr::select(any_of(c("plot_id", "fire_frequency") ) )
-
-### transforming in matrix
-plot_fire_mtx = as.matrix(plot_fire[,-1])
-rownames(plot_fire_mtx) = plot_fire$plot_id
 
 ### fire per plot
 plot_env = comm_data %>% 
@@ -147,8 +149,7 @@ plot_env = comm_data %>%
   dplyr::select(any_of(c("plot_id", 
                          "fire_frequency", 
                          "seasonal_precipitation",
-                         "soil_PC1",
-                         "soil_PC2"
+                         "soil_PC1"
                          ) 
                        ) 
                 )
@@ -157,17 +158,30 @@ plot_env = comm_data %>%
 plot_env_mtx = as.matrix(plot_env[,-1])
 rownames(plot_env_mtx) = plot_env$plot_id
 
+### coordinates per plot
+plot_coords = comm_data %>% 
+  unite("plot_id", site, plot, sep="_", remove = FALSE) %>% 
+  select("plot_id", "longitude", "latitude")
+
+### transforming in matrix
+plot_coords_mtx = as.matrix(plot_coords[,-1])
+rownames(plot_coords_mtx) = plot_coords$plot_id
 
 ### dissimilarity matrices
-spp_dist = distance(plot_spp_mtx, "sorensen")
-fire_dist = distance(plot_fire_mtx, "euclidean")
-env_dist = distance(plot_env_mtx, "euclidean")
+spp_dist = xdiss(plot_spp_mtx, method = "bray")
+env_dist = dist(plot_env_mtx, method = "euclidean")
+geo_dist = dist(plot_coords_mtx, method = "euclidean")
 
-ape::mantel.test(spp_dist, env_dist, 
-                 nperm = 999,
-                 alternative = "greater",
-                 graph = T)
+### MANTEL
+mantel_test = vegan::mantel.partial(
+                      xdis = env_dist,
+                      ydis = spp_dist,
+                      zdis = geo_dist,
+                      method="pearson", 
+                      permutations=9999
+                      )
 
+plot(env_dist, spp_dist)
 
-
+### dist by dist plot
 
